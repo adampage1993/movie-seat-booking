8728d5ba3bf80c582f63f8e5d8c75ec21f3a9f6a
adampage1993 <63195949+adampage1993@users.noreply.github.com>
2020-08-03 12:45:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 // SELECTORS
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seats:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
 // FUNCTIONS
-// Update Total And Count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll(".row .seat.selected");
-
-    const selectedSeatsCount = selectedSeats.length;
-
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 // EVENT LISTENERS
-// Movie Select Event
-movieSelect.addEventListener("change", function (e) {
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
     updateSelectedCount();
 })
-
-// Seat Click Event
-container.addEventListener("click", function (e) {
-    if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
-        e.target.classList.toggle("selected");
-
-        updateSelectedCount();
-    }
-});
